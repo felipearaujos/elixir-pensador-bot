66f7eb893d10576aad360c527eec9e9b16a4d246
Felipe Araujo <felipe.araujo.s@live.com>
2020-06-08 22:43:05 -0300

    Refactor to a simple approach without Spider

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,11 +1,10 @@
-#https://www.erlang-solutions.com/blog/web-scraping-with-elixir.html
-
 defmodule PensadorCrawler.Worker do
-  @behaviour Crawly.Spider
-
-
   def start() do
-    Crawly.Engine.start_spider(PensadorCrawler.Worker)
+    build_url()
+    |> fetch
+    |> parse_response
+    |> extract_data
+    |> get_random_phrase
   end
 
   def randomize() do
@@ -13,74 +12,34 @@ defmodule PensadorCrawler.Worker do
   end
 
   def randomize(number) do
-    IO.inspect(number, label: "number")
     case number do
       1 -> randomize()
       _ -> number
     end
   end
 
-
-  @impl Crawly.Spider
-  def base_url() do
-    "https://www.pensador.com"
-  end
-
-  @impl Crawly.Spider
-  def init() do
+  def build_url() do
     page = randomize()
-    url = "https://www.pensador.com/frases/#{page}/"
-
-    IO.inspect(url)
-    [      
-      start_urls: ["https://www.pensador.com/frases/10/"]
-    ]
+    "https://www.pensador.com/frases/#{page}/"
   end
 
-  @impl Crawly.Spider
-  def parse_item(response) do
+  def fetch(url) do
+    {:ok, response} = Crawly.fetch(url)
+    response
+  end
 
-    parsed_body = Floki.parse(response.body)  
-    phrases_list = Floki.find(parsed_body,"[id=phrasesList] > .thought-card")
+  def parse_response(response) do
+    Floki.find(response.body, "[id=phrasesList] > .thought-card")
+  end
 
-    bob = Enum.reduce(phrases_list, [], fn(phrase_element, acc) ->
-      
-      data_id = Floki.attribute(phrase_element, "data-id")
+  def extract_data(phrases_list) do
+    Enum.reduce(phrases_list, [], fn phrase_element, acc ->
       data_alt = Floki.attribute(phrase_element, "data-alt")
-
-      elem = %{id: data_id, data_alt: data_alt}
-      IO.inspect(acc)
-      
-      [ elem | acc]
+      [data_alt | acc]
     end)
-    
-
-    # # get the blog_post
-    # blog_post = Floki.find(parsed_body, "article.blog_post")
-
-    # # Extract item from a page, e.g.
-    # # https://www.erlang-solutions.com/blog/introducing-telemetry.html
-    # title =
-    #   blog_post
-    #   |> Floki.find("h1:first-child")
-    #   |> Floki.text()
-
-    # author =
-    #   blog_post
-    #   |> Floki.find("p.subheading")
-    #   |> Floki.text(deep: false, sep: "")
-    #   |> String.trim_leading()
-    #   |> String.trim_trailing()
-
-    # text =
-    #   blog_post
-    #   |> Floki.text()
-
-    foi =  Enum.at(bob, :rand.uniform(Enum.count(bob)))
-     %Crawly.ParsedItem{
-       :items => [foi]
-     }
   end
 
-
+  def get_random_phrase(parsed_phrases) do
+    Enum.at(parsed_phrases, :rand.uniform(Enum.count(parsed_phrases)))
+  end
 end
